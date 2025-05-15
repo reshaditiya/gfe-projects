1f14f0efd261fcb1cb87b57d0c2fe00aee210bdd
Resha Aditiya <reshaditiya@gmail.com>
2025-05-16 01:23:10 +0700

    chore: remove unnecessary addon storybook

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -4,9 +4,7 @@ const config: StorybookConfig = {
   stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
   addons: [
     '@storybook/addon-essentials',
-    '@storybook/addon-onboarding',
-    '@chromatic-com/storybook',
-    '@storybook/experimental-addon-test',
+    '@storybook/addon-a11y',
   ],
   framework: {
     name: '@storybook/nextjs',
