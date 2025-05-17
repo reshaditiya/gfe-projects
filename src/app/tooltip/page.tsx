8f973e76df05963a738df813b79e2950fa700726
Resha Aditiya <reshaditiya@gmail.com>
2025-05-17 12:03:52 +0700

    style: adjust tooltip page according to design

--- src/app/tooltip/page.tsx
+++ src/app/tooltip/page.tsx
@@ -7,12 +7,14 @@ import {
 export default function Page() {
   return (
     <main className="flex min-h-screen items-center justify-center">
-      <Tooltip open>
-        <TooltipTrigger className="invisible h-[1px] w-[1px]">
-          Text
-        </TooltipTrigger>
-        <TooltipContent>This is a tooltip</TooltipContent>
-      </Tooltip>
+      <div className="mt-15.5">
+        <Tooltip open>
+          <TooltipTrigger className="invisible h-[1px] w-[1px]">
+            Text
+          </TooltipTrigger>
+          <TooltipContent>This is a tooltip</TooltipContent>
+        </Tooltip>
+      </div>
     </main>
   );
 }
